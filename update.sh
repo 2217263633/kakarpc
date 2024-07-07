171516f8015959ebe6399308124e80c63c648962
kasia <2217263633@qq.com>
2024-07-07 18:10:45 +0800

    llok

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ string=`git describe --abbrev=0`
 version=$(echo "$string" | awk -F. '{print $1"."$2"."$3+1}')
 echo $version
 git add .
-# git commit -m "update version to $version"
-git tag $version -m "update version to $version"
+git commit -m "update version to $version"
+git tag $version -m "$version"
 git push --tags
 sleep  10s
