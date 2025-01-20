c78c71c0b22597051016fb3e175fabfb86eee4c4
kasia <2217263633@qq.com>
2025-01-20 10:11:18 +0800

    update version to v0.0.112

--- cusrequest/main.go
+++ cusrequest/main.go
@@ -59,8 +59,39 @@ func Request(_url string, method PolicyType, data any, Authorization string) (ma
 		return resu, err
 	}
 
-	// logger.Info(methods[method], payload, string(body))
+	err = json.Unmarshal(body, &resu)
+	return resu, err
+}
 
+func RequestWithHeader(_url string, method PolicyType, data any, headers http.Header) (map[string]interface{}, error) {
+	var resu map[string]interface{} = make(map[string]interface{})
+	jsonVal, err := json.Marshal(data)
+	payload := strings.NewReader(string(jsonVal))
+	if err != nil {
+		payload = nil
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(methods[method], _url, payload)
+	if err != nil {
+		return resu, err
+	}
+	req.Header = headers
+	res, err := client.Do(req)
+	if err != nil {
+		return resu, err
+	}
+
+	if res.StatusCode != 200 {
+		err = fmt.Errorf("status code is %d", res.StatusCode)
+		return resu, err
+	}
+
+	defer res.Body.Close()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return resu, err
+	}
 	err = json.Unmarshal(body, &resu)
 	return resu, err
 }
