418b49a1fad187382c75d652e72d34d02d6b0951
kasia <2217263633@qq.com>
2025-03-21 12:07:43 +0800

    update version to v0.0.115

--- cusrequest/main.go
+++ cusrequest/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type PolicyType int32
@@ -94,4 +95,46 @@ func RequestWithHeader(_url string, method PolicyType, data any, headers http.He
 	}
 	err = json.Unmarshal(body, &resu)
 	return resu, err
+}
+
+// return string error
+func Request2(_url string, method PolicyType, data any, Authorization string) (string, error) {
+	var resu string = ""
+	jsonVal, err := json.Marshal(data)
+	payload := strings.NewReader(string(jsonVal))
+
+	if err != nil {
+		payload = nil
+	}
+
+	client := &http.Client{
+		Timeout: time.Second * 25,
+	}
+	req, err := http.NewRequest(methods[method], _url, payload)
+	if err != nil {
+
+		return resu, err
+	}
+	req.Header.Add("Authorization", Authorization)
+	req.Header.Add("User-Agent", "Apifox/1.0.0 (https://apifox.com)")
+	if method != Get {
+		req.Header.Add("Content-Type", "application/json")
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return resu, err
+	}
+
+	if res.StatusCode != 200 {
+		return resu, fmt.Errorf(fmt.Sprintf("status code is %d", res.StatusCode))
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return resu, err
+	}
+	return string(body), nil
 }
